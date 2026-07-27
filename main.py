f2f400d7abaf063f588a19befc8187bfacdd3a87
kauanzin04 <kauanrodriguesdasilva04@gmail.com>
2026-07-27 00:00:46 -0300

    projeto completo de API com GET/POST/PUT/DELETE

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
 from fastapi import FastAPI
+from pydantic import BaseModel
 import uvicorn
 
 app = FastAPI(
@@ -8,20 +8,45 @@ app = FastAPI(
     version="1.0.0",
 )
 
-mensagem = os.environ.get("apareceu", "olá, mundo!")  # Valor padrão caso a variável de ambiente não esteja definida
+class usuario(BaseModel):
+    nome: str
+    idade: int
+    email: str
 
+usuarios = []
+
+#GET
 @app.get("/")
-def read_root():
-    return {"message": mensagem}
+def inicio():
+    return {"message": "API funcionando!"}
 
-@app.get("/status")
-def read_status():
-    return {"status": "API está funcionando corretamente."}
+#GET buscando dados
+@app.get("/usuarios")
+def listar_usuarios():
+    return usuarios
+
+#POST criando dados
+@app.post("/usuarios")
+def criar_usuario(usuario: usuario):
+    usuarios.append(usuario)
+    return {"message": "Usuário criado com sucesso!", "usuario": usuario}
+
+#PUT atualizando dados
+@app.put("/usuarios/{id}")
+def atualizar_usuario(id: int, usuario: usuario):
+    usuarios[id] = usuario
+    return {"message": "Usuário atualizado com sucesso!", "usuario": usuario}
+
+#DELETE deletando dados
+@app.delete("/usuarios/{id}")
+def deletar_usuario(id: int):
+    usuario = usuarios.pop(id)
+    return {"message": "Usuário deletado com sucesso!", "usuario": usuario}
 
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="127.0.0.1",
         port=8000,
-        reload=True,
+        reload=True
     )
